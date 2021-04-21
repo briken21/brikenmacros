9a44620fe9586fe94a445429f967f754fed32bc7
MaxPallas <81631933+MaxPallas@users.noreply.github.com>
2021-04-21 12:18:24 +0200

    Delete useranalysis/OnlineBRIKEN2021 directory

--- useranalysis/OnlineBRIKEN2021/onlinelib/setuponline.sh
+++
@@ -1,5 +0,0 @@
-export ONLINE_MAIN=/home/maxpallas/data/briken2021/OnlineLibraries
-export OFFLINE_MAIN=/home/maxpallas/data/briken2021/OnlineLibraries
-export PATH=$ONLINE_MAIN/bin:$PATH
-export LD_LIBRARY_PATH=$ONLINE_MAIN/lib
-export ROOT_INCLUDE_PATH=$ONLINE_MAIN/include:$ONLINE_MAIN/include/Event:$ONLINE_MAIN/include/pmonitor
